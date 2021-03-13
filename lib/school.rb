007917b68e2ed995fd8e753e725d6e5fea681e37
Lrd134 <larryc3200@gmail.com>
2021-03-13 12:09:56 -0500

    commit tree d952561503ed725b029397f77bf5d300c16e3bfc

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
+require 'pry'
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] = [] if @roster[grade].nil?
+        
+        
+        @roster.map do |key, value|
+            if key == grade
+                value << student_name
+            end
+        end
+    end
+    def grade(number)
+        
+        @roster.each do | key, value |
+            if key == number
+                return value
+            end
+        end
+    end
+    def sort
+        
+        @roster.each { | key, value | value.sort! }
+        @roster.sort.to_h
+    end
+
+end
+
+# var1 = School.new("EAMS")
+
+# var1.add_student("Larry", 16)
